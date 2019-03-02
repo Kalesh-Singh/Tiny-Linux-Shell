3f3bf48a785d72ee192a2f39e40c8f13566e26ee
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-03-02 13:59:12 -0800

    Updated docs

--- tsh.c
+++ tsh.c
@@ -36,8 +36,8 @@
  *                       stdout.
  *
  * Each job can be identified by either a process ID (PID) or a
- * job ID (JID), which is a positive integer assigned by tsh. J
- * IDs should be denoted on the command line by the prefix ‘%’.
+ * job ID (JID), which is a positive integer assigned by tsh.
+ * JIDs should be denoted on the command line by the prefix ‘%’.
  *
  * For example, “%5” denotes JID 5, and “5” denotes PID 5.
  */
